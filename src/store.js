71be3e05d4bf7ffe2fbd30493fbf9d8c4a18a6ab
Sudesh Gutta <sudesh2610@gmail.com>
2019-03-24 16:16:01 +0530

    Added support for redux dev tools

--- src/store.js
+++ src/store.js
@@ -1,7 +1,12 @@
 import { createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
+import { composeWithDevTools } from "redux-devtools-extension/developmentOnly";
 import rootReducer from "./reducers";
 
 export default function configureStore(initialState = {}) {
-  return createStore(rootReducer, initialState, applyMiddleware(thunk));
+  return createStore(
+    rootReducer,
+    initialState,
+    composeWithDevTools(applyMiddleware(thunk))
+  );
 }
