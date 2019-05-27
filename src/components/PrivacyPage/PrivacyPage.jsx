6eb293fd30ef5a134069b68009403d55ff6bc9c7
Karthik Narumanchi <karthiknr@gmail.com>
2019-05-27 17:38:00 +0530

    Temporary manual privacy page content

--- src/components/PrivacyPage/PrivacyPage.jsx
+++ src/components/PrivacyPage/PrivacyPage.jsx
@@ -4,8 +4,19 @@ import { connect } from "react-redux";
 import PropTypes from "prop-types";
 
 import CloudCircle from "@material-ui/icons/CloudCircle";
-import { AppBar, Typography, Link, Toolbar } from "@material-ui/core";
-import { WEBSITE_PRIVACY_URL, GITHUB_ORG_URL } from "../../actions/constants";
+import {
+  AppBar,
+  Typography,
+  Link,
+  Toolbar,
+  IconButton
+} from "@material-ui/core";
+import {
+  WEBSITE_PRIVACY_URL,
+  GITHUB_ORG_URL,
+  WEBSITE_PUBLIC_URL,
+  FEEDBACK_URL
+} from "../../actions/constants";
 
 const styles = theme => ({
   root: {
@@ -19,17 +30,31 @@ const styles = theme => ({
   },
   appBarMain: {
     flexGrow: 0,
-    width: "100%"
+    width: "100%",
+    zIndex: "100"
+  },
+  appBarCustom: {
+    backgroundColor: theme.palette.common.white,
+    color: theme.palette.primary.main,
+    boxShadow: "1"
   },
   mainContent: {
-    width: "100%"
-  },
-  subMainContent: {
+    width: "100%",
     display: "flex",
     flexDirection: "column",
     justifyContent: "center",
     alignItems: "center",
-    alignContent: "center"
+    alignContent: "center",
+    marginTop: "10%"
+  },
+  subMainContent: {
+    width: "75%",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+    alignContent: "center",
+    margin: theme.spacing.unit * 1
   },
   footer: {
     flexGrow: 0,
@@ -77,9 +102,50 @@ const styles = theme => ({
     color: theme.palette.secondary.main,
     paddingLeft: theme.spacing.unit * 1,
     paddingRight: theme.spacing.unit * 1
+  },
+  CookieDeclarationType: {
+    margin: "12px 0 12px 0",
+    padding: "8px 8px 0 8px",
+    border: "1px solid #333333",
+    verticalAlign: "top"
+  },
+
+  CookieDeclarationTypeHeader: {
+    fontWeight: "bold"
+  },
+
+  CookieDeclarationTypeDescription: {
+    margin: "2px 0 16px 0"
+  },
+
+  CookieDeclarationTable: {
+    tableLayout: "fixed",
+    width: "100%",
+    borderCollapse: "collapse",
+    borderSpacing: "0",
+    margin: "0 0 18px 0",
+    padding: "0",
+    border: "0",
+    fontSize: "100%",
+    verticalAlign: "baseline"
+  },
+
+  CookieDeclarationTableHeader: {
+    fontWeight: "bold",
+    borderBottom: "1px solid #777777",
+    textAlign: "left",
+    padding: "4px"
+  },
+
+  CookieDeclarationTableCell: {
+    textOverflow: "ellipsis",
+    wordWrap: "break-word",
+    borderBottom: "1px solid #777777",
+    verticalAlign: "top",
+    padding: "4px 4px 5px 4px"
   }
 });
-
+/* eslint-disable no-script-url */
 // eslint-disable-next-line react/prefer-stateless-function
 class PrivacyPage extends React.Component {
   render() {
@@ -88,36 +154,264 @@ class PrivacyPage extends React.Component {
     return (
       <div className={classes.root}>
         <div className={classes.appBarMain}>
-          <AppBar position="static">
+          <AppBar position="static" className={classes.appBarCustom}>
             <Toolbar className={classes.appBar}>
               <div className={classes.appBarLeft}>
-                <CloudCircle className={classes.icon} />
-                <Typography variant="h6" color="inherit" noWrap>
-                  Collab
-                </Typography>
+                <Link href={WEBSITE_PUBLIC_URL} rel="noopener">
+                  <IconButton color="primary" aria-label="Homepage">
+                    <CloudCircle />
+                  </IconButton>
+                </Link>
+                <Link href={WEBSITE_PUBLIC_URL} rel="noopener">
+                  <Typography variant="h5" color="inherit" noWrap>
+                    Collab
+                  </Typography>
+                </Link>
               </div>
-              <div className={classes.appBarRight} />
             </Toolbar>
           </AppBar>
         </div>
-
         <div className={classes.mainContent}>
           <div className={classes.subMainContent}>
-            <Typography variant="h5" color="inherit">
-              Cookie Declaration
+            <Typography
+              variant="h4"
+              color="inherit"
+              align="center"
+              gutterBottom
+            >
+              Cookies Declaration
             </Typography>
-            <script
-              id="CookieDeclaration"
-              src="https://consent.cookiebot.com/22183745-e911-4700-8e51-eecbd220b50f/cd.js"
-              type="text/javascript"
-              async
-            />
+
+            <Typography
+              variant="body1"
+              color="inherit"
+              align="center"
+              gutterBottom
+            >
+              This website uses cookies. We use cookies to analyse our traffic.
+              We also share information about your use of our site with our
+              analytics partners who may combine it with other information that
+              you’ve provided to them or that they’ve collected from your use of
+              their services. You consent to our cookies if you continue to use
+              our website.
+            </Typography>
+            <Typography
+              variant="body1"
+              color="inherit"
+              align="center"
+              gutterBottom
+            >
+              This site uses different types of cookies. Some cookies are placed
+              by third party services that appear on our pages.
+            </Typography>
+            <Typography
+              variant="body1"
+              color="inherit"
+              align="center"
+              gutterBottom
+            >
+              You can at any time change or withdraw your consent from the
+              Cookie Declaration on our website in our Privacy Policy.
+            </Typography>
+            <Typography
+              variant="body1"
+              color="inherit"
+              align="center"
+              gutterBottom
+            >
+              Your consent applies to the following domains: collabhq.app
+            </Typography>
+            <Typography
+              variant="h6"
+              color="inherit"
+              align="center"
+              gutterBottom
+            >
+              Manage your consent
+            </Typography>
+            <Link color="secondary" href={"javascript:CookieConsent.renew();"}>
+              <Typography
+                color="secondary"
+                variant="subtitle1"
+                align="center"
+                gutterBottom
+              >
+                Change your consent
+              </Typography>
+            </Link>
+            <Link
+              color="secondary"
+              href={"javascript:CookieConsent.withdraw();"}
+            >
+              <Typography
+                color="secondary"
+                variant="subtitle1"
+                align="center"
+                gutterBottom
+              >
+                Withdraw your consent
+              </Typography>
+            </Link>
+
+            <div className={classes.CookieDeclarationType} lang="en" dir="ltr">
+              <p className={classes.CookieDeclarationTypeHeader}>Necessary</p>
+              <p className={classes.CookieDeclarationTypeDescription}>
+                Necessary cookies help make a website usable by enabling basic
+                functions like page navigation and access to secure areas of the
+                website. The website cannot function properly without these
+                cookies.
+              </p>
+              <table className={classes.CookieDeclarationTable}>
+                <colgroup>
+                  <col style={{ width: "20%" }} />
+                  <col style={{ width: "20%" }} />
+                  <col style={{ width: "35%" }} />
+                  <col style={{ width: "15%" }} />
+                  <col style={{ width: "10%" }} />
+                </colgroup>
+                <thead>
+                  <tr>
+                    <th
+                      scope="col"
+                      className={classes.CookieDeclarationTableHeader}
+                      dir="ltr"
+                    >
+                      Name
+                    </th>
+                    <th
+                      scope="col"
+                      className={classes.CookieDeclarationTableHeader}
+                      dir="ltr"
+                    >
+                      Provider
+                    </th>
+                    <th
+                      scope="col"
+                      className={classes.CookieDeclarationTableHeader}
+                      dir="ltr"
+                    >
+                      Purpose
+                    </th>
+                    <th
+                      scope="col"
+                      className={classes.CookieDeclarationTableHeader}
+                      dir="ltr"
+                    >
+                      Expiry
+                    </th>
+                    <th
+                      scope="col"
+                      className={classes.CookieDeclarationTableHeader}
+                      dir="ltr"
+                    >
+                      Type
+                    </th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td className={classes.CookieDeclarationTableCell}>
+                      CookieConsent
+                    </td>
+                    <td className={classes.CookieDeclarationTableCell}>
+                      collabhq.app
+                    </td>
+                    <td className={classes.CookieDeclarationTableCell}>
+                      Stores the user's cookie consent state for the current
+                      domain
+                    </td>
+                    <td className={classes.CookieDeclarationTableCell}>
+                      1 year
+                    </td>
+                    <td className={classes.CookieDeclarationTableCell}>
+                      HTTP Cookie
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+            <div className={classes.CookieDeclarationType} lang="en" dir="ltr">
+              <p className={classes.CookieDeclarationTypeHeader}>
+                Unclassified
+              </p>
+              <p className={classes.CookieDeclarationTypeDescription}>
+                Unclassified cookies are cookies that we are in the process of
+                classifying, together with the providers of individual cookies.
+              </p>
+              <table className={classes.CookieDeclarationTable}>
+                <colgroup>
+                  <col style={{ width: "20%" }} />
+                  <col style={{ width: "20%" }} />
+                  <col style={{ width: "35%" }} />
+                  <col style={{ width: "15%" }} />
+                  <col style={{ width: "10%" }} />
+                </colgroup>
+                <thead>
+                  <tr>
+                    <th
+                      scope="col"
+                      className={classes.CookieDeclarationTableHeader}
+                      dir="ltr"
+                    >
+                      Name
+                    </th>
+                    <th
+                      scope="col"
+                      className={classes.CookieDeclarationTableHeader}
+                      dir="ltr"
+                    >
+                      Provider
+                    </th>
+                    <th
+                      scope="col"
+                      className={classes.CookieDeclarationTableHeader}
+                      dir="ltr"
+                    >
+                      Purpose
+                    </th>
+                    <th
+                      scope="col"
+                      className={classes.CookieDeclarationTableHeader}
+                      dir="ltr"
+                    >
+                      Expiry
+                    </th>
+                    <th
+                      scope="col"
+                      className={classes.CookieDeclarationTableHeader}
+                      dir="ltr"
+                    >
+                      Type
+                    </th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td className={classes.CookieDeclarationTableCell}>
+                      persist:root
+                    </td>
+                    <td className={classes.CookieDeclarationTableCell}>
+                      collabhq.app
+                    </td>
+                    <td className={classes.CookieDeclarationTableCell}>
+                      Stores the application business logic state
+                    </td>
+                    <td className={classes.CookieDeclarationTableCell}>
+                      Persistent
+                    </td>
+                    <td className={classes.CookieDeclarationTableCell}>
+                      HTML Local Storage
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
           </div>
         </div>
         <footer className={classes.footer}>
           <div className={classes.footerSubContent}>
             <Link
-              color="inherit"
+              color="secondary"
               href={WEBSITE_PRIVACY_URL}
               target="_blank"
               rel="noopener"
@@ -132,7 +426,7 @@ class PrivacyPage extends React.Component {
               </Typography>
             </Link>
             <Link
-              color="inherit"
+              color="secondary"
               href={GITHUB_ORG_URL}
               target="_blank"
               rel="noopener"
@@ -144,6 +438,21 @@ class PrivacyPage extends React.Component {
                 className={classes.footerSubText}
               >
                 Github
+              </Typography>
+            </Link>
+            <Link
+              color="secondary"
+              href={FEEDBACK_URL}
+              target="_blank"
+              rel="noopener"
+            >
+              <Typography
+                variant="subtitle2"
+                align="center"
+                gutterBottom
+                className={classes.footerSubText}
+              >
+                Feedback
               </Typography>
             </Link>
             <Typography
