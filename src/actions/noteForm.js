ee44e61818e23c180dcb2add070adcdc4b39e031
Karthik Narumanchi <karthiknr@gmail.com>
2019-07-27 12:07:34 +0200

    Added complete Markdown support

--- src/actions/noteForm.js
+++ src/actions/noteForm.js
@@ -3,6 +3,7 @@
  */
 export const SET_TITLE = "SET_TITLE";
 export const SET_CONTENT = "SET_CONTENT";
+export const SET_TAB = "SET_TAB";
 
 /**
  * Action Creators
@@ -12,3 +13,6 @@ export const setTitle = title => dispatch =>
 
 export const setContent = content => dispatch =>
   dispatch({ type: SET_CONTENT, payload: content });
+
+export const setTab = tab => dispatch =>
+  dispatch({ type: SET_TAB, payload: tab });
